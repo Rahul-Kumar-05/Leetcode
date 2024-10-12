b5ed5293292252500a7c77a9e165f0f648551ac3
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2024-10-13 00:21:10 +0530

    Time: 2 ms (90.88%) | Memory: 133.9 MB (49.31%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -3,7 +3,7 @@ class Solution {
         
         int first = Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
-        int third = Integer.MAX_VALUE;
+        // int third = Integer.MAX_VALUE;
 
         for(int i=0; i<nums.length; i++){
             int element = nums[i];
@@ -17,7 +17,7 @@ class Solution {
             }
 
             else{
-                third = element;
+                // third = element;
                 return true;
             }
         }
