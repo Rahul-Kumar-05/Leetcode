59c70a0da06ea6e4a578cb690aa6d7b043d972ba
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2024-12-20 03:50:59 +0530

    Time: 21 ms (25.36%) | Memory: 45.3 MB (21.15%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,17 +1,49 @@
 class Solution {
     public int countSubstrings(String s) {
+
+        // Memonization 
+        // int n = s.length();
+
+        // int dp[][] = new int[n+1][n+1];
+        // for(int i=0; i<dp.length; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+
+        // int count = 0;
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=i; j<n; j++){
+        //         if(check(i, j, s, dp) == true){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+
+        // ------------------------------------------------------------
+
+        // Tabulation 
         int n = s.length();
-
-        int dp[][] = new int[n+1][n+1];
-        for(int i=0; i<dp.length; i++){
-            Arrays.fill(dp[i], -1);
-        }
-
         int count = 0;
 
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(check(i, j, s, dp) == true){
+        boolean dp[][] = new boolean[n][n];
+
+        for(int l=1; l<=n; l++){
+            for(int i=0; i+l-1<n; i++){
+                int j = i+l-1;
+
+                if(i == j){
+                    dp[i][j] = true;
+                }
+                else if(i+1 == j){
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                }
+                else{
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i+1][j-1]);
+                }
+
+                if(dp[i][j]){
                     count++;
                 }
             }
@@ -20,22 +52,22 @@ class Solution {
         return count;
     }
 
-    public static boolean check(int i, int j, String s, int dp[][]){
-        if(i > j){
-            return true;
-        }
+    // public static boolean check(int i, int j, String s, int dp[][]){
+    //     if(i > j){
+    //         return true;
+    //     }
 
-        if(dp[i][j] != -1){
-            return (dp[i][j] == 1);
-        }
+    //     if(dp[i][j] != -1){
+    //         return (dp[i][j] == 1);
+    //     }
 
-        if(s.charAt(i) == s.charAt(j)){
-            boolean result = check(i+1, j-1, s, dp);
-            dp[i][j] = result ? 1 : 0;
-            return result;
-        }
+    //     if(s.charAt(i) == s.charAt(j)){
+    //         boolean result = check(i+1, j-1, s, dp);
+    //         dp[i][j] = result ? 1 : 0;
+    //         return result;
+    //     }
 
-        dp[i][j] = 0;
-        return false;
-    }
+    //     dp[i][j] = 0;
+    //     return false;
+    // }
 }
