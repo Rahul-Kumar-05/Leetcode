eb2178907df825e968881fa4076d8cf92e994fb1
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2024-12-22 16:22:17 +0530

    Time: 9 ms (64.18%) | Memory: 45.4 MB (13.92%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,10 +1,25 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         
-        return helper(nums, target, 0, 0);
+        int  n = nums.length;
+        int sum = 0;
+
+        for(int i=0; i<nums.length; i++){
+            sum = sum + nums[i];
+        }        
+
+        int dp[][] = new int[n][2*sum + 1];
+
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[i].length; j++){
+                dp[i][j] = -1;
+            }
+        }
+
+        return helper(nums, target, 0, 0, sum, dp);
     }
 
-    public int helper(int nums[], int target, int idx, int currsum){
+    public int helper(int nums[], int target, int idx, int currsum, int sum, int dp[][]){
         
         if(idx == nums.length){
             if(target == currsum){
@@ -15,9 +30,13 @@ class Solution {
             }
         }
 
-        int addcase = helper(nums, target, idx+1, currsum + nums[idx]);
-        int minuscase = helper(nums, target, idx+1, currsum - nums[idx]);
+        if(dp[idx][sum + currsum] != -1){
+            return dp[idx][sum + currsum];
+        }
 
-        return addcase + minuscase;
+        int addcase = helper(nums, target, idx+1, currsum + nums[idx], sum, dp);
+        int minuscase = helper(nums, target, idx+1, currsum - nums[idx], sum, dp);
+
+        return dp[idx][sum + currsum] = addcase + minuscase;
     }
 }
