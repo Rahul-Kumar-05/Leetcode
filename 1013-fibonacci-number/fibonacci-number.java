f41dbf879c98a5abff4af1a2fb5f178165a172a6
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2024-12-23 00:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (50.80%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,29 +1,29 @@
 class Solution {
     public int fib(int n) {
         
-        // int dp[] = new int[n+1];
-        // for(int i=0; i<dp.length; i++){
-        //     dp[i] = -1;
-        // }
+        int dp[] = new int[n+1];
+        for(int i=0; i<dp.length; i++){
+            dp[i] = -1;
+        }
 
-        // return helper(n, dp);
+        return helper(n, dp);
 
 
         // Tabulation approach
 
-        if(n==1 || n==0){
-            return n;
-        }
+        // if(n==1 || n==0){
+        //     return n;
+        // }
         
-        int dp[] = new int [n+1];
-        dp[0] = 0;
-        dp[1] = 1;
+        // int dp[] = new int [n+1];
+        // dp[0] = 0;
+        // dp[1] = 1;
 
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
+        // for(int i=2; i<=n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
 
-        return dp[n];
+        // return dp[n];
     }
 
     public int helper(int n, int dp[]){
@@ -35,6 +35,6 @@ class Solution {
             return dp[n];
         }
 
-        return dp[n] = fib(n-1) + fib(n-2);
+        return dp[n] = helper(n-1, dp) + helper(n-2, dp);
     }
 }
