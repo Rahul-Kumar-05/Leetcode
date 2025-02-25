9d8fdecdbf06a0507f2d9e455103559e096225e7
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-02-26 02:16:45 +0530

    Time: 1 ms (99.86%) | Memory: 46.8 MB (5.72%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,22 +1,35 @@
 class Solution {
     public int singleNumber(int[] nums) {
     
-        int res = 0;
-        for(int i=0; i<nums.length; i++){
+    //  BRUTE FORCE APPROACH
+
+        // int res = 0;
+        // for(int i=0; i<nums.length; i++){
             
-            int curr = nums[i];
-            int count = 0;
-            for(int j=0; j<nums.length; j++){
-                if(nums[j] == curr){
-                    count++;
-                }
-            }
+        //     int curr = nums[i];
+        //     int count = 0;
+        //     for(int j=0; j<nums.length; j++){
+        //         if(nums[j] == curr){
+        //             count++;
+        //         }
+        //     }
 
-            if(count == 1){
-                res = nums[i];
-            }
-        }
+        //     if(count == 1){
+        //         res = nums[i];
+        //     }
+        // }
 
-        return res;
+        // return res;
+
+
+
+    //  OPTIMAL SOLUTION USING XOR
+
+        int xor = 0;
+        for(int i=0; i<nums.length; i++){
+            xor = xor ^ nums[i];
+        } 
+
+        return xor;
     }
 }
