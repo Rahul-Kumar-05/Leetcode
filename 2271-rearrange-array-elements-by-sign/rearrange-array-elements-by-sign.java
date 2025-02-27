338cb7c518b1f2f7fc777edda88ee649e056c0b3
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-02-28 03:18:17 +0530

    Time: 3 ms (100.00%) | Memory: 77.2 MB (49.00%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -7,28 +7,28 @@ class Solution {
 
         int result[] = new int[n];
 
-        // for(int num : nums){
-        //     if(num > 0){
-        //         result[posIdx] = num;
-        //         posIdx += 2;
-        //     }
-        //     else{
-        //         result[negIdx] = num;
-        //         negIdx += 2;
-        //     }
-        // }
-
-        for(int i=0; i<n; i++){
-
-            if(nums[i] > 0){
-                result[posIdx] = nums[i];
+        for(int num : nums){
+            if(num > 0){
+                result[posIdx] = num;
                 posIdx += 2;
             }
             else{
-                result[negIdx] = nums[i];
-                negIdx +=2;
+                result[negIdx] = num;
+                negIdx += 2;
             }
         }
+
+        // for(int i=0; i<n; i++){
+
+        //     if(nums[i] > 0){
+        //         result[posIdx] = nums[i];
+        //         posIdx += 2;
+        //     }
+        //     else{
+        //         result[negIdx] = nums[i];
+        //         negIdx +=2;
+        //     }
+        // }
 
         return result;
     }
