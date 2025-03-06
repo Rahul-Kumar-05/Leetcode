20755e41f5ab30e04f40ac879869b12378de8117
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-03-06 20:19:21 +0530

    Time: 26 ms (24.98%) | Memory: 50.8 MB (66.66%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -10,26 +10,48 @@ class Solution {
             }
         }
 
-        return LCS(text1, text2, n, m, dp);
+        return lcsHelper(text1, text2, n, m, dp);
     }
 
-    public static int LCS(String s1, String s2, int n, int m, int dp[][]){
+    public static int lcsHelper(String s1, String s2, int n, int m, int dp[][]){
+        
         if(n == 0 || m == 0){
             return 0;
         }
-
+        
         if(dp[n][m] != -1){
             return dp[n][m];
         }
-
+        
+        int take = 0;
         if(s1.charAt(n-1) == s2.charAt(m-1)){
-            dp[n][m] = 1 + LCS(s1, s2, n-1, m-1, dp); 
+            take = 1 + lcsHelper(s1, s2, n-1, m-1, dp);
         }
-
         else{
-            dp[n][m] = Math.max(LCS(s1, s2, n-1, m, dp), LCS(s1, s2, n, m-1, dp));
+            take = Math.max(lcsHelper(s1, s2, n-1, m, dp), lcsHelper(s1, s2, n, m-1, dp));
         }
-
-        return dp[n][m];
+        
+        dp[n][m] = take;
+        return take;
     }
+
+    // public static int LCS(String s1, String s2, int n, int m, int dp[][]){
+    //     if(n == 0 || m == 0){
+    //         return 0;
+    //     }
+
+    //     if(dp[n][m] != -1){
+    //         return dp[n][m];
+    //     }
+
+    //     if(s1.charAt(n-1) == s2.charAt(m-1)){
+    //         dp[n][m] = 1 + LCS(s1, s2, n-1, m-1, dp); 
+    //     }
+
+    //     else{
+    //         dp[n][m] = Math.max(LCS(s1, s2, n-1, m, dp), LCS(s1, s2, n, m-1, dp));
+    //     }
+
+    //     return dp[n][m];
+    // }
 }
