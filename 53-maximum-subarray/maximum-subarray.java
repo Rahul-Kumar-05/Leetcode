d55704417e58472b0dc528f24420c121bb65b875
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-03-10 10:50:45 +0530

    Time: 1 ms (99.49%) | Memory: 57.2 MB (42.65%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -7,23 +7,23 @@ class Solution {
         for(int i=0; i<arr.length; i++){
             curr_sum = curr_sum + arr[i];
 
-            if (curr_sum < 0){
-                curr_sum = 0;
-            }
-
             if (curr_sum > max_sum){
                 max_sum = curr_sum;
             }
+
+            if (curr_sum < 0){
+                curr_sum = 0;
+            }
         }  
 
-        if (max_sum == 0){
-            max_sum = arr[0];
-            for(int i=0; i<arr.length; i++){
-                if(arr[i] > max_sum){
-                    max_sum = arr[i];
-                }
-            }
-        }
+        // if (max_sum == 0){
+        //     max_sum = arr[0];
+        //     for(int i=0; i<arr.length; i++){
+        //         if(arr[i] > max_sum){
+        //             max_sum = arr[i];
+        //         }
+        //     }
+        // }
         return max_sum;
         
     }
