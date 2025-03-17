2a2f6dc9bac0675af3fcdaa196ca230ba4191625
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-03-17 20:05:42 +0530

    Time: 8 ms (40.30%) | Memory: 44.5 MB (58.82%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -5,11 +5,27 @@ class Solution {
             return false;
         }
 
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        for(int i=0; i<nums.length; i+=2){
+        // for(int i=0; i<nums.length; i+=2){
 
-            if(nums[i] != nums[i+1]){
+        //     if(nums[i] != nums[i+1]){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for(int count : mp.values()){
+
+            if(count % 2 != 0){
                 return false;
             }
         }
