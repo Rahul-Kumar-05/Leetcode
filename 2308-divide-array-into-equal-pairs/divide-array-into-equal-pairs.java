127e6bdafe87d78f296050a14bbbc0431d57c123
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-03-17 20:08:02 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (58.82%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean divideArray(int[] nums) {
         
-        if((nums.length % 2) != 0){
-            return false;
-        }
+        // if((nums.length % 2) != 0){
+        //     return false;
+        // }
 
         // Arrays.sort(nums);
 
@@ -16,16 +16,31 @@ class Solution {
 
         // return true;
 
-        HashMap<Integer, Integer> mp = new HashMap<>();
+        // HashMap<Integer, Integer> mp = new HashMap<>();
+
+        // for(int i=0; i<nums.length; i++){
+
+        //     mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+        // }
+
+        // for(int count : mp.values()){
+
+        //     if(count % 2 != 0){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+        int freq[] = new int[501];
 
         for(int i=0; i<nums.length; i++){
-
-            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+            freq[nums[i]]++;
         }
 
-        for(int count : mp.values()){
+        for(int i=0; i<501; i++){
 
-            if(count % 2 != 0){
+            if(freq[i] % 2 != 0){
                 return false;
             }
         }
