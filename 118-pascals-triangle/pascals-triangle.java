2db31e1558d177373ce672a1c31b4a8e7a125a67
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-03-19 02:17:37 +0530

    Time: 1 ms (83.88%) | Memory: 42 MB (78.27%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -8,7 +8,7 @@ class Solution {
         pascal[0][0] = 1;
 
         for(int i=1; i<numRows; i++){
-            for(int j=0; j<numRows; j++){
+            for(int j=0; j<=i; j++){
                 
                 if(j-1 < 0){
                     pascal[i][j] = pascal[i-1][j];
@@ -22,8 +22,8 @@ class Solution {
         for(int i=0; i<numRows; i++){
             List<Integer> row = new ArrayList<>();
 
-            for(int j=0; j<numRows; j++){
-                if(pascal[i][j] != 0)
+            for(int j=0; j<=i; j++){
+                // if(pascal[i][j] != 0)
                     row.add(pascal[i][j]);
             }
 
