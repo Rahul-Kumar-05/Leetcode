e366f1024496e0bbb4759c06e6661db211b9d088
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-03-24 15:44:08 +0530

    Time: 6 ms (70.74%) | Memory: 44.8 MB (55.51%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,30 +1,56 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         
-        ArrayList<Character> list = new ArrayList<>();
+        // ArrayList<Character> list = new ArrayList<>();
 
-        int i = 0;
-        int j = 0;
+        // int i = 0;
+        // int j = 0;
+
+        // int max = 0;
+        // int count = 0;
+
+        // while(i<s.length()-max && j<s.length()){
+
+        //     if(list.contains(s.charAt(j))){
+        //         max = Math.max(max, count);
+        //         count = 0;
+        //         i++;
+        //         j = i;
+        //         list.clear();
+        //     }
+        //     else{
+        //         list.add(s.charAt(j));
+        //         count++;
+        //         j++;
+        //     }
+        // }
+
+        // return Math.max(max, count);
+
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+        int right = 0;
 
         int max = 0;
         int count = 0;
 
-        while(i<s.length()-max && j<s.length()){
+        for(int i=right; i<s.length(); i++){
+            
+            char c = s.charAt(i);
 
-            if(list.contains(s.charAt(j))){
-                max = Math.max(max, count);
-                count = 0;
-                i++;
-                j = i;
-                list.clear();
+            while(set.contains(c)){
+                set.remove(s.charAt(left));
+                left++;
+                count--;
             }
-            else{
-                list.add(s.charAt(j));
-                count++;
-                j++;
-            }
+
+            set.add(c);
+            right++;
+            count = right - left;
+            max = Math.max(max, count);
         }
 
-        return Math.max(max, count);
+        return max;
     }
 }
