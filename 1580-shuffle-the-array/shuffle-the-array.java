052e517025bab70da6dd54de0e3e63b2f926509d
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-03-25 11:39:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (76.58%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,27 +1,11 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-
-        int left[] = new int[n];
-        int right[] = new int[n];
-
-        for(int i=0; i<n; i++){
-            left[i] = nums[i];
-        }
         
-        int k = 0;
-        for(int i=n; i<nums.length; i++){
-            right[k] = nums[i];
-            k++;
-        }
-
         int ans[] = new int[2*n];
 
-        int l = 0;
-        for(int i=0; i<ans.length; i+=2){
-            ans[i] = left[l];
-            ans[i+1] = right[l];
-
-            l++;
+        for(int i=0; i<n; i++){
+            ans[2*i] = nums[i];
+            ans[2*i+1] = nums[n+i];
         }
 
         return ans;
