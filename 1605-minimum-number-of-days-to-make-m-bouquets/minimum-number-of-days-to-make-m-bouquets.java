3c3704ef82ea79cb1762e30d48936746795c38c5
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-04-02 21:56:29 +0530

    Time: 22 ms (33.83%) | Memory: 59.6 MB (5.54%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,52 +1,47 @@
-
 class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        long mul = (long) m*k;
+        if(mul > bloomDay.length){
+            return -1;
+        }
+    
+        int l = Arrays.stream(bloomDay).min().getAsInt();
+        int h =Arrays.stream(bloomDay).max().getAsInt();
 
-    public static boolean isValid(int arr[], int day, int m, int k){
+        while(l<=h){
+            int mid = l+ (h-l)/2;
 
-        int flower = 0;
-        int book = 0;
-
-        for(int i : arr){
-            if(i <= day){
-                flower++;
-
-                if(flower == k){
-                    flower = 0;
-                    book++;
-                }
+            if(isvalid(bloomDay,mid,m,k)){
+                h = mid-1;
             }
             else{
-                flower = 0;
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+
+    public boolean isvalid(int[] arr, int mid, int m, int k){
+        int flower =0;
+        int boo = 0;
+
+        for(int i : arr){
+            if(i<=mid){
+                flower++;
+                if(flower==k){
+                    flower=0;
+                     boo++;
+              }
+            }
+            else{
+                flower =0;
             }
 
-            if(book >= m){
+            if(boo==m){
                 return true;
             }
         }
 
         return false;
-    }
-
-    public int minDays(int[] bloomDay, int m, int k) {
-        long mul = (long)m*k;
-        if(mul > bloomDay.length){
-            return -1;
-        }
-
-        int l = Arrays.stream(bloomDay).min().getAsInt();
-        int h = Arrays.stream(bloomDay).max().getAsInt();
-
-        while(l < h){
-            int mid = l + (h-l)/2;
-
-            if(isValid(bloomDay, mid, m, k)){
-                h = mid;
-            }
-            else{
-                l = mid + 1;
-            }
-        }
-        
-        return l;
     }
 }
