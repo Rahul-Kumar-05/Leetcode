54d71e712996ea73fe7ffcc12591b98e79a204ae
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-04-05 01:58:34 +0530

    Time: 42 ms (72.69%) | Memory: 56 MB (48.85%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -3,44 +3,43 @@ class Solution {
     public static boolean isValid(int arr[], int mid, int m){
 
         int prev = arr[0];
-        int countBall = 1;
+        int currcount = 1;
 
         for(int i=1; i<arr.length; i++){
             int curr = arr[i];
 
-            if(Math.abs(curr - prev) >= mid){
-                countBall++;
+            if(curr - prev >= mid){
+                currcount++;
                 prev = curr;
             }
 
-            if(countBall == m){
+            if(currcount == m){
                 break;
             }
         }
 
-        return (countBall == m);
+        return (currcount == m);
     }
 
     public int maxDistance(int[] position, int m) {
-
         Arrays.sort(position);
-        
+
         int minForce = 1;
         int maxForce = position[position.length-1] - position[0];
 
-        int result = 0;
+        int res = 0;
         while(minForce <= maxForce){
-            int mid = minForce + (maxForce - minForce)/2;
+            int mid = minForce + (maxForce-minForce)/2;
 
             if(isValid(position, mid, m)){
-                result = mid;
-                minForce = mid + 1;
+                res = mid;
+                minForce = mid+1;
             }
             else{
                 maxForce = mid-1;
             }
         }
 
-        return result;
+        return res;
     }
 }
