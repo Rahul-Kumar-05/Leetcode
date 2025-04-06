08eb6bb5d90d52295ff449dffbc2ab01477169d7
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-04-06 14:52:21 +0530

    Time: 3 ms (90.19%) | Memory: 43.5 MB (63.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,11 +3,14 @@ class Solution {
         
         if(s.length() != t.length())  return false;
 
+        char ch1[] = s.toCharArray();
+        char ch2[] = t.toCharArray();
+
         int freq[] = new int[26];
 
-        for(int i=0; i<s.length(); i++){
-            freq[s.charAt(i)-97]++;
-            freq[t.charAt(i)-97]--;
+        for(int i=0; i<ch1.length; i++){
+            freq[ch1[i]-'a']++;
+            freq[ch2[i]-'a']--;
         }
 
         for(int i=0; i<freq.length; i++){
