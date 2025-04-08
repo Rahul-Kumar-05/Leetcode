4cbe86040507594e34e17bd99255ff494e4eb50d
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-04-08 22:49:41 +0530

    Time: 5 ms (51.20%) | Memory: 47.2 MB (25.60%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -9,50 +9,53 @@
  * }
  */
 class Solution {
+
+    public static ListNode reverse(ListNode head){
+        
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
     public ListNode doubleIt(ListNode head) {
         
-        ListNode temp = head;
-
-        int size = 0;
-        while(temp != null){
-            size++;
-            temp = temp.next;
-        }
-
-        temp = head;
-        int arr[] = new int[size+1];
-
-        for(int i=1; i<arr.length; i++){
-            arr[i] = temp.val;
-            temp = temp.next;
-        }
+        ListNode thead = reverse(head);
+        ListNode t = thead;
 
         int carry = 0;
-        for(int i=arr.length-1; i>=0; i--){
-            int digit = arr[i] * 2 + carry;
+        while(t != null){
+            int num = (t.val * 2) + carry;
 
-            arr[i] = digit % 10;
-            carry = 0;
+            int digit = num % 10;
+            t.val = digit;
 
-            if(digit > 9){
-                carry = digit / 10;
-            }
+            carry = num / 10;
 
-        }
-
-        ListNode t = new ListNode(0);
-        ListNode head1 = t;
-
-        if(arr[0] != 0){
-            t.next = new ListNode(arr[0]);
             t = t.next;
         }
 
-        for(int i=1; i<arr.length; i++){
-            t.next = new ListNode(arr[i]);
+        t = thead;
+
+        while(t.next != null){
             t = t.next;
         }
 
-        return head1.next;
+        if(carry > 0){
+            t.next = new ListNode(carry);
+            t = t.next;
+        }
+
+        ListNode res = reverse(thead);
+
+        return res;
     }
 }
