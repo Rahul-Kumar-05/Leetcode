0e8e39318448b332e2082fa455041acfeba064c5
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-04-08 17:42:37 +0530

    Time: 1 ms (98.39%) | Memory: 44.8 MB (23.39%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -3,25 +3,14 @@ class Solution {
 
         int freq[] = new int[101];
 
-        // int last = 0;
-        int idx = 0;
         for(int i=nums.length-1; i>=0; i--){
             if(freq[nums[i]] > 0){
-                idx = i;
-
                 return (int) Math.ceil((double)(i + 1)/3);
-            }
-            // last++;
+            } 
+
             freq[nums[i]]++;
         }
 
-        // // double remaining = nums.length - last;
-        // double remaining = idx + 1;
-
-        // double ans = Math.ceil(remaining/3); 
-        // System.out.println(ans);
-
-        // return (int)ans;
         return 0;
     }
 }
