4c8e97fb547a279b5d9bd484ddd5974b5602e84a
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-04-09 20:05:49 +0530

    Time: 658 ms (5.01%) | Memory: 42.8 MB (98.90%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,53 +1,53 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         
-        // ArrayList<Character> list = new ArrayList<>();
+        ArrayList<Character> list = new ArrayList<>();
 
-        // int i = 0;
-        // int j = 0;
-
-        // int max = 0;
-        // int count = 0;
-
-        // while(i<s.length()-max && j<s.length()){
-
-        //     if(list.contains(s.charAt(j))){
-        //         max = Math.max(max, count);
-        //         count = 0;
-        //         i++;
-        //         j = i;
-        //         list.clear();
-        //     }
-        //     else{
-        //         list.add(s.charAt(j));
-        //         count++;
-        //         j++;
-        //     }
-        // }
-
-        // return Math.max(max, count);
-
-        HashSet<Character> set = new HashSet<>();
-
-        int left = 0;
+        int i = 0;
+        int j = 0;
 
         int max = 0;
         int count = 0;
 
-        for(int right=0; right<s.length(); right++){
-            
-            char c = s.charAt(right);
+        while( j<s.length()){
 
-            while(set.contains(c)){
-                set.remove(s.charAt(left));
-                left++;
+            if(list.contains(s.charAt(j))){
+                max = Math.max(max, count);
+                count = 0;
+                i++;
+                j = i;
+                list.clear();
             }
-
-            set.add(c);
-            count = right - left + 1;
-            max = Math.max(max, count);
+            else{
+                list.add(s.charAt(j));
+                count++;
+                j++;
+            }
         }
 
-        return max;
+        return Math.max(max, count);
+
+        // HashSet<Character> set = new HashSet<>();
+
+        // int left = 0;
+
+        // int max = 0;
+        // int count = 0;
+
+        // for(int right=0; right<s.length(); right++){
+            
+        //     char c = s.charAt(right);
+
+        //     while(set.contains(c)){
+        //         set.remove(s.charAt(left));
+        //         left++;
+        //     }
+
+        //     set.add(c);
+        //     count = right - left + 1;
+        //     max = Math.max(max, count);
+        // }
+
+        // return max;
     }
 }
