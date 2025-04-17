9627a6c279590e11078d1de78d66d14fb74cca97
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-04-17 11:01:01 +0530

    Time: 55 ms (89.90%) | Memory: 42.7 MB (76.30%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,46 +1,20 @@
-class Solution {
-
-    // public static int getMax(int arr[]){
-    //     int max = 0;
-
-    //     for(int i=0; i<arr.length; i++){
-    //         max = Math.max(max, arr[i]);
-    //     }
-
-    //     return max;
-    // }
-
-    // public static int getMin(int arr[]){
-    //     int min = Integer.MAX_VALUE;
-
-    //     for(int i=0; i<arr.length; i++){
-    //         if(arr[i] != 0){
-    //             min = Math.min(min, arr[i]);
-    //         }
-    //     }
-
-    //     return min;
-    // }
-
+class Solution {    
     public int beautySum(String s) {
         
         char ch[] = s.toCharArray();
         int sum = 0;
 
-        for(int i=0; i<ch.length; i++){
-            
+        for(int i=0; i<ch.length; i++){            
             int freq[] = new int[26];
+
             for(int j=i; j<ch.length; j++){
                 
                 freq[ch[j] - 'a']++;
 
-                // int beauty = getMax(freq) - getMin(freq);
-                // sum += beauty;
-
                 int max = 0;
                 int min = Integer.MAX_VALUE;
 
-                for(int k=0; k<freq.length; k++){
+                for(int k=0; k<26; k++){
                     max = Math.max(max, freq[k]);
 
                     if(freq[k] != 0){
