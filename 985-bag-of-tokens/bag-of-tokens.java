df94ec9167fa15a846c71d6ed5d9085676950ea4
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-05-18 10:53:18 +0530

    Time: 2 ms (100.00%) | Memory: 43.2 MB (15.69%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        
+        Arrays.sort(tokens);
+
+        int res = 0;
+
+        int left = 0;
+        int right = tokens.length-1;
+
+        while(left < right){
+            if(tokens[left] <= power){
+                res++;
+                power -= tokens[left];
+                left++;
+            }
+
+            else if(res > 0){
+                res--;
+                power += tokens[right];
+                right--;
+            }
+
+            else{
+                left++;
+            }
+        }
+
+        if(left == right && tokens[left] <= power){
+            res++;
+        }
+
+        return res;
+    }
+}
