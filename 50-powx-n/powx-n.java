e6acad95c20692eefc35c232c99a9fdf0cd2d1ae
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-05-26 10:47:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (53.15%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,37 @@
+class Solution {
+    public static double getPower(double a, long b){
+
+        if(b == 0){
+            return 1;
+        }
+
+        double half = getPower(a, b/2);
+        double res = half * half;
+
+        if(b % 2 == 1){
+            return a * res;
+        }
+
+        return res;
+    }
+
+
+
+    public double myPow(double x, int n) {
+        
+        double res = 0;
+
+        if(n >= 0){
+            long m = n;
+            res = getPower(x, m);
+        }
+        else{
+            long m = -(long)n;
+
+            double r = getPower(x, m);
+            res = 1 / r;
+        }
+
+        return res;
+    }
+}
