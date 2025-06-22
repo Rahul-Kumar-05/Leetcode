9628165d4e7a1fbfa74773bcc14fe117a34ccd5d
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-06-22 11:58:45 +0530

    Time: 1 ms (93.67%) | Memory: 42.3 MB (74.40%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
@@ -14,29 +14,6 @@ class Solution {
         String arr[] = new String[size];
 
         int idx = 0;
-        // for(int i=0; i<size; i++){
-        //     if(idx + k > n){
-        //         StringBuilder sb = new StringBuilder();
-        //         int j = n-idx;
-
-        //         while(j > 0){
-        //             sb.append(s.charAt(idx));
-        //             idx++;
-        //             j--;
-        //         }
-
-        //         int remaining = k - (n % k);
-        //         while(remaining > 0){
-        //             sb.append(fill);
-        //             remaining--;
-        //         }
-
-        //         arr[i] = sb.toString();
-        //         break;
-        //     }
-        //     arr[i] = s.substring(idx, idx+k);
-        //     idx = idx + k;
-        // }
 
         for(int i=0; i<size; i++){
             StringBuilder sb = new StringBuilder();
@@ -48,7 +25,7 @@ class Solution {
                 }
                 else{
                     sb.append(fill);
-                    idx++;
+                    // idx++;
                 }
             }
 
