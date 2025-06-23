931b5791386c963777f1168fbf9f1a8072fe4b22
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-06-23 20:48:40 +0530

    Time: 71 ms (92.59%) | Memory: 77.1 MB (23.70%) - LeetSync

---
+++ 307-range-sum-query-mutable/range-sum-query-mutable.java
@@ -0,0 +1,67 @@
+class NumArray {
+    int nums[];
+    int bit[];
+    int n;
+
+    public NumArray(int[] nums) {
+        this.nums = nums;
+
+        int n = nums.length; 
+
+        bit = new int[n+1];
+
+        for(int i=0; i<nums.length; i++){
+            build(i, nums[i]);
+        }
+    }
+
+    public void build(int i, int val){
+        i = i+1;
+
+        while(i < bit.length){
+            bit[i] += val;
+
+            i = i + (i & (-i));
+        }
+    }
+    
+    public void update(int index, int val) {
+        
+        int diff = val - nums[index];
+
+        nums[index] = val;
+
+        index = index + 1;
+
+        while(index < bit.length){
+            bit[index] += diff;
+
+            index = index + (index & (-index));
+        }
+    }
+
+    public int range(int i){
+
+        i = i+1;
+        int res = 0;
+
+        while(i > 0){
+            res += bit[i];
+
+            i = i - (i & (-i));
+        }
+
+        return res;
+    }
+    
+    public int sumRange(int left, int right) {
+        return range(right) - range(left-1);
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
