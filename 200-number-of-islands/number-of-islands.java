ed14256e35ce342f61f6542e3aea0f0d19107cf9
Rahul kumar <160847295+Rahul-Kumar-05@users.noreply.github.com>
2025-07-07 15:04:30 +0530

    Time: 5 ms (33.92%) | Memory: 52.5 MB (14.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,37 +1,60 @@
 class Solution {
 
-    public static void bfs(char[][] grid, int row, int col, int n, int m){
+    static int dir_row[] = {-1, 0, 0, 1};
+    static int dir_col[] = {0, -1, 1, 0};
 
-        if(row < 0 || col < 0 || row >= n || col >=m || grid[row][col] == '0'){
-            return;
+    static class Pair {
+        int row;
+        int col;
+
+        Pair(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
-
-        grid[row][col] = '0';
-
-        bfs(grid, row-1, col, n, m);
-        bfs(grid, row, col+1, n, m);
-        bfs(grid, row+1, col, n, m);
-        bfs(grid, row, col-1, n, m);        
     }
 
-    public int numIslands(char[][] grid) {
-        
-        int n = grid.length;
-        int m = grid[0].length;
 
-        int count = 0;
+    public static void BFS(int i, int j, char grid[][], int vis[][]){
+        vis[i][j] = 1;
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(i, j));
 
-                if(grid[i][j] == '1'){
-                    bfs(grid, i, j, n, m);
-                    count++;
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int row = p.row;
+            int col = p.col;
+            
+            for(int k=0; k<4; k++){
+                int newrow = row + dir_row[k];
+                int newcol = col + dir_col[k];
+
+                if(newrow >= 0 && newrow < grid.length && newcol >= 0 && newcol < grid[0].length && grid[newrow][newcol] == '1' && vis[newrow][newcol] == 0){
+                    vis[newrow][newcol] = 1;
+                    q.offer(new Pair(newrow, newcol));
                 }
             }
         }
+    }
 
-        return count;
+    public int numIslands(char[][] grid) {
+        int row = grid.length; 
+        int col = grid[0].length;
 
+        int vis[][] = new int[row][col];
+
+        int no_of_islands = 0;
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                
+                if(grid[i][j] == '1' && vis[i][j] == 0){
+                    BFS(i, j, grid, vis);
+                    no_of_islands++;
+                }
+            }
+        } 
+
+        return no_of_islands;
     }
 }
